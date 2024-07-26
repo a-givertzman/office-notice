1ff665998b9a558d9236bd289b2abc51751b44f1
a-givertzman <a.givertzman@icloud.com>
2024-07-26 23:37:17 +0300

    static analyser fixes

--- src/states.rs
+++ src/states.rs
@@ -339,7 +339,7 @@ pub async fn callback(bot: Bot, q: CallbackQuery, dialogue: MyDialogue, state: S
             }
         }
         State::Links(state) => {
-            log::debug!("states.callback | Links > state: {:#?}", state);
+            // log::debug!("states.callback | Links > state: {:#?}", state);
             let input = q.data.to_owned().unwrap_or_default();
             log::debug!("states.callback | Links > Input: {}", input);
             let cmd = LinksMenu::parse(&input, 0);
